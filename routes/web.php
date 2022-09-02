7cd172ee27cc35188b2e624b8b0a84efe5f58616
EC2 Default User <ec2-user@ip-172-31-67-185.ec2.internal>
2022-09-02 04:54:41 +0000

    formテスト用のコードを書いた

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\TestController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,10 +17,14 @@ use App\Http\Controllers\TaskController;
 |
 */
 
-//タスク管理システム
+// タスク管理システム
 Route::get('/', [AuthController::class, 'index']);
 Route::get('/task/list', [TaskController::class, 'list']);
 
-//テスト用
-Route::get('/welcome/', [WelcomeController::class, 'index']);
+
+// テスト用
+Route::get('/welcome', [WelcomeController::class, 'index']);
 Route::get('/welcome/second', [WelcomeController::class, 'second']);
+// form入力テスト用
+Route::get('/test', [TestController::class, 'index']);
+Route::post('/test/input', [TestController::class, 'input']);
