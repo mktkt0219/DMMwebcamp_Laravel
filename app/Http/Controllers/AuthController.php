70ad1394ad11405738ae6e2742ad7a10f52eb58a
EC2 Default User <ec2-user@ip-172-31-67-185.ec2.internal>
2022-09-02 05:07:39 +0000

    認証の「formデータを受け取る」所まで書いた

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginPostRequest; 
 
 class AuthController extends Controller
 {
@@ -14,5 +15,15 @@ class AuthController extends Controller
     public function index()
     {
         return view('index');
+    }
+    
+    public function login(LoginPostRequest $request){
+        // validate済
+
+        // データの取得
+        $datum = $request->validated();
+
+        //
+        var_dump($datum); exit;
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,8 +2,16 @@
 
 @section('contets')
         <h1>ログイン</h1>
-         <form action="/login" method="post">
-            email：<input name="email"><br>
+        @if ($errors->any())
+            <div>
+            @foreach ($errors->all() as $error)
+                {{ $error }}<br>
+            @endforeach
+            </div>
+        @endif
+        <form action="/login" method="post">
+            @csrf
+            email：<input name="email" value="{{ old('email') }}"><br>
             パスワード：<input  name="password" type="password"><br>
             <button>ログインする</button>
         </form>
