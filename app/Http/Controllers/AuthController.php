707abf8283c17e28a9ce92f6839cd98cee36caf8
EC2 Default User <ec2-user@ip-172-31-67-185.ec2.internal>
2022-09-02 06:38:21 +0000

    認証と認可の実装まで書いた

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,7 +3,9 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\LoginPostRequest; 
+use App\Http\Requests\LoginPostRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
@@ -16,14 +18,41 @@ class AuthController extends Controller
     {
         return view('index');
     }
-    
-    public function login(LoginPostRequest $request){
+
+    /**
+     * ログイン処理
+     * 
+     */
+    public function login(LoginPostRequest $request)
+    {
         // validate済
 
         // データの取得
         $datum = $request->validated();
+        //var_dump($datum); exit;
+
+        // 認証
+        if (Auth::attempt($datum) === false) {
+            return back()
+                   ->withInput() // 入力値の保持
+                   ->withErrors(['auth' => 'emailかパスワードに誤りがあります。',]) // エラーメッセージの出力
+                   ;
+        }
 
         //
-        var_dump($datum); exit;
+        $request->session()->regenerate();
+        return redirect()->intended('/task/list');
+    }
+
+    /**
+     * ログアウト処理
+     * 
+     */
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->regenerateToken();  // CSRFトークンの再生成
+        $request->session()->regenerate();  // セッションIDの再生成
+        return redirect(route('front.index'));
     }
 }
