8c507a2db0f8d101b5a9454f554938b8df5db7c8
EC2 Default User <ec2-user@ip-172-31-67-185.ec2.internal>
2022-09-05 02:45:30 +0000

    タスクの詳細閲覧を書いた

--- routes/web.php
+++ routes/web.php
@@ -22,9 +22,13 @@ Route::get('/', [AuthController::class, 'index'])->name('front.index');
 Route::post('/login', [AuthController::class, 'login']);
 // 認可処理
 Route::middleware(['auth'])->group(function () {
-    Route::get('/task/list', [TaskController::class, 'list']);
+    Route::prefix('/task')->group(function () {
+        Route::get('/list', [TaskController::class, 'list']);
+        Route::post('/register', [TaskController::class, 'register']);
+        Route::get('/detail/{task_id}', [TaskController::class, 'detail'])->whereNumber('task_id')->name('detail');
+    });
+    //
     Route::get('/logout', [AuthController::class, 'logout']);
-    Route::post('/task/register', [TaskController::class, 'register']);
 });
 
 
